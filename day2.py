bb41e848bcb313edced20c9ff727c5a486c043d0
VaibhaK25 <kondlevibes@gmail.com>
2026-07-26 10:07:30 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,11 @@
+
+sentence = "Basic programming skills developed"         
+age = 18              
+height = 4.9          
+is_student = True 
+vowels="aeiouAEIOU"
+c=0
+for i in sentence:
+    if i in vowels:
+        c=c+1
+print(c)
